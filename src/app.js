04ad9f9c98fa05540a6661c68a48069ea1ab3a34
Jonas Castro <jonascastro128@gmail.com>
2020-06-16 11:08:39 -0300

    CRUD Repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,54 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repositorie);
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const indexRepositorie = repositories.findIndex(repositorie => repositorie.id === id)
+  if (indexRepositorie === -1) return response.status(400).json();
+
+  const repositorie = { id, title, url, techs, likes: repositories[indexRepositorie].likes }
+  repositories[indexRepositorie] = repositorie;
+
+  response.json(repositorie)
+
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const indexRepositorie = repositories.findIndex(repositorie => repositorie.id === id)
+  if (indexRepositorie === -1) return response.status(400).json();
+
+  repositories.splice(indexRepositorie, 1)
+  return response.status(204).json()
+
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorie = repositories.find(repositorie => repositorie.id === id);
+  if (!repositorie) return response.status(400).json();
+
+  repositorie.likes += 1
+
+  return response.json(repositorie)
+
 });
 
 module.exports = app;
